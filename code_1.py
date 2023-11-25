183266e8effc83dfe4989bbf82bef317598a3e26
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-11-25 19:39:02 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,30 @@
+import random
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    data = generate_random_data()
+
+
+        print(f"Random Number: {item}")
+    main()
+    for item in data:
+
+
+    return data
+def main():    }
+}
+
+}
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+    return data;
+    });
+main();
+
+function main() {
+    const data = [];
+    data.forEach(item => {
+
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
